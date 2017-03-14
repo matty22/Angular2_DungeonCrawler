52d67f4884a7b6cfd1c064ce30a6400cafbeca5f
Matt <matt.leonard.22@gmail.com>
2017-03-14 15:44:36 -0700

    wall generation and player movement and wall collision detection added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,27 +15,29 @@ export class AppComponent implements OnInit {
   canvasHeight: number;
   playerLocation: number[];
   enemyLocation: number[];
+  wallTiles: Array<Array<number>> = [];
   player:Player = {
-    health: 50,
-    xp: 0,
-    minAttack: 1,
-    maxAttack: 3,
-    weapon: "Iron Sword"
-  }
+          health: 50,
+          xp: 0,
+          minAttack: 1,
+          maxAttack: 3,
+          weapon: "Iron Sword",
+          hasKey: false
+        }
   enemy: Enemy = {
-  health: 10,
-  minAttack: 3,
-  maxAttack: 4
-  }
-
+          health: 10,
+          minAttack: 3,
+          maxAttack: 4
+        }
 
   ngOnInit() {
     this.canvas = <HTMLCanvasElement> document.getElementById('dungeonMap');
     this.ctx = this.canvas.getContext('2d');
-    this.canvasWidth = 100;
-    this.canvasHeight = 75;
+    this.canvasWidth = 860;
+    this.canvasHeight = 640;
     this.buildBoard();
-    // this.placePlayer();
+    this.buildWalls();
+    this.placePlayer();
     // this.placeEnemies();
   }
 
@@ -45,9 +47,142 @@ export class AppComponent implements OnInit {
       for (var i = 0; i < this.canvasWidth; i++) {
         this.ctx.fillStyle = "gray";
         this.ctx.fillRect(i * 10, j * 10, 10, 10);
-        this.ctx.strokeStyle="#5a5a5a";
-            this.ctx.strokeRect(i * 10, j * 10, 10, 10);
       }
+    }
+  }
+
+  buildWalls() {
+    this.buildVerticalWalls();
+    this.buildHorizontalWalls();
+  }
+
+  // Builds Vertical Walls
+  buildVerticalWalls() {
+    for(let i = 0; i < 9; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(20 * 10, i * 10, 10, 10);
+      this.wallTiles.push([20, i]);
+      this.ctx.fillRect(21 * 10, i * 10, 10, 10);
+      this.wallTiles.push([21, i]);
+      this.ctx.fillRect(42 * 10, i * 10, 10, 10);
+      this.wallTiles.push([42, i]);
+      this.ctx.fillRect(43 * 10, i * 10, 10, 10);
+      this.wallTiles.push([43, i]);
+      this.ctx.fillRect(64 * 10, i * 10, 10, 10);
+      this.wallTiles.push([64, i]);
+      this.ctx.fillRect(65 * 10, i * 10, 10, 10);
+      this.wallTiles.push([65, i]);
+    }
+
+    for (let i = 11; i < 31; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(20 * 10, i * 10, 10, 10);
+      this.wallTiles.push([20, i]);
+      this.ctx.fillRect(21 * 10, i * 10, 10, 10);
+      this.wallTiles.push([21, i]);
+      this.ctx.fillRect(42 * 10, i * 10, 10, 10);
+      this.wallTiles.push([42, i]);
+      this.ctx.fillRect(43 * 10, i * 10, 10, 10);
+      this.wallTiles.push([43, i]);
+      this.ctx.fillRect(64 * 10, i * 10, 10, 10);
+      this.wallTiles.push([64, i]);
+      this.ctx.fillRect(65 * 10, i * 10, 10, 10);
+      this.wallTiles.push([65, i]);
+    }
+    
+    for (let i = 33; i < 53; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(20 * 10, i * 10, 10, 10);
+      this.wallTiles.push([20, i]);
+      this.ctx.fillRect(21 * 10, i * 10, 10, 10);
+      this.wallTiles.push([21, i]);
+      this.ctx.fillRect(42 * 10, i * 10, 10, 10);
+      this.wallTiles.push([42, i]);
+      this.ctx.fillRect(43 * 10, i * 10, 10, 10);
+      this.wallTiles.push([43, i]);
+      this.ctx.fillRect(64 * 10, i * 10, 10, 10);
+      this.wallTiles.push([64, i]);
+      this.ctx.fillRect(65 * 10, i * 10, 10, 10);
+      this.wallTiles.push([65, i]);
+    }
+
+    for (let i = 55; i < 64; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(20 * 10, i * 10, 10, 10);
+      this.wallTiles.push([20, i]);
+      this.ctx.fillRect(21 * 10, i * 10, 10, 10);
+      this.wallTiles.push([21, i]);
+      this.ctx.fillRect(42 * 10, i * 10, 10, 10);
+      this.wallTiles.push([42, i]);
+      this.ctx.fillRect(43 * 10, i * 10, 10, 10);
+      this.wallTiles.push([43, i]);
+      this.ctx.fillRect(64 * 10, i * 10, 10, 10);
+      this.wallTiles.push([64, i]);
+      this.ctx.fillRect(65 * 10, i * 10, 10, 10);
+      this.wallTiles.push([65, i]);
+    }
+  }
+
+  // Builds horizontal Walls
+  buildHorizontalWalls() {
+    for(let i = 0; i < 9; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(i * 10, 20 * 10, 10, 10);
+      this.wallTiles.push([i, 20]);
+      this.ctx.fillRect(i * 10, 21 * 10, 10, 10);
+      this.wallTiles.push([i, 21]);
+      this.ctx.fillRect(i * 10, 42 * 10, 10, 10);
+      this.wallTiles.push([i, 42]);
+      this.ctx.fillRect(i * 10, 43 * 10, 10, 10);
+      this.wallTiles.push([i, 43]);
+    }
+
+    for(let i = 11; i < 31; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(i * 10, 20 * 10, 10, 10);
+      this.wallTiles.push([i, 20]);
+      this.ctx.fillRect(i * 10, 21 * 10, 10, 10);
+      this.wallTiles.push([i, 21]);
+      this.ctx.fillRect(i * 10, 42 * 10, 10, 10);
+      this.wallTiles.push([i, 42]);
+      this.ctx.fillRect(i * 10, 43 * 10, 10, 10);
+      this.wallTiles.push([i, 43]);
+    }
+
+    for(let i = 33; i < 53; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(i * 10, 20 * 10, 10, 10);
+      this.wallTiles.push([i, 20]);
+      this.ctx.fillRect(i * 10, 21 * 10, 10, 10);
+      this.wallTiles.push([i, 21]);
+      this.ctx.fillRect(i * 10, 42 * 10, 10, 10);
+      this.wallTiles.push([i, 42]);
+      this.ctx.fillRect(i * 10, 43 * 10, 10, 10);
+      this.wallTiles.push([i, 43]);
+    }
+
+    for(let i = 55; i < 75; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(i * 10, 20 * 10, 10, 10);
+      this.wallTiles.push([i, 20]);
+      this.ctx.fillRect(i * 10, 21 * 10, 10, 10);
+      this.wallTiles.push([i, 21]);
+      this.ctx.fillRect(i * 10, 42 * 10, 10, 10);
+      this.wallTiles.push([i, 42]);
+      this.ctx.fillRect(i * 10, 43 * 10, 10, 10);
+      this.wallTiles.push([i, 43]);
+    }
+
+    for(let i = 77; i < 86; i++) {
+      this.ctx.fillStyle = "black";
+      this.ctx.fillRect(i * 10, 20 * 10, 10, 10);
+      this.wallTiles.push([i, 20]);
+      this.ctx.fillRect(i * 10, 21 * 10, 10, 10);
+      this.wallTiles.push([i, 21]);
+      this.ctx.fillRect(i * 10, 42 * 10, 10, 10);
+      this.wallTiles.push([i, 42]);
+      this.ctx.fillRect(i * 10, 43 * 10, 10, 10);
+      this.wallTiles.push([i, 43]);
     }
   }
 
@@ -71,64 +206,82 @@ export class AppComponent implements OnInit {
   // Move player upon key press
   movePlayer(pressedKey) {
       switch (pressedKey.keyCode) {
-        case 37:
         // Player pushes left arrow key
-        if(this.playerLocation[0] === (this.enemyLocation[0] + 1) && this.playerLocation[1] === this.enemyLocation[1]) {
-          console.log("enemy hit");
-        } else {
-          this.ctx.fillStyle = "black";
+        case 37:
+          // Fill in the space player is leaving with gray background color
+          this.ctx.fillStyle = "gray";
           this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-          this.playerLocation[0] = this.playerLocation[0] - 1;
-          this.ctx.fillStyle = "blue";
-          this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-        }
+          this.playerLocation[0] -= 1;
+          // If the space the player is moving into is a wall, move player back to left 1 space
+          if (JSON.stringify(this.wallTiles).indexOf(JSON.stringify(this.playerLocation)) !== -1 || this.playerLocation[0] == -1) {
+            this.playerLocation[0] += 1;
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
+          // If the space the player is moving into is not a wall, move player into that new space 
+          else {
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
           break;
-        case 38:
+
         // Player pushes up arrow key
-        if(this.playerLocation[1] === (this.enemyLocation[1] + 1) && this.playerLocation[0] === this.enemyLocation[0]) {
-          console.log("enemy hit");
-        } else {
-          this.ctx.fillStyle = "black";
+        case 38:
+          // Fill in the space player is leaving with gray background color
+          this.ctx.fillStyle = "gray";
           this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-          this.playerLocation[1] = this.playerLocation[1] - 1;
-          this.ctx.fillStyle = "blue";
-          this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-        }
+          this.playerLocation[1] -= 1;
+          // If the space the player is moving into is a wall, move player back to left 1 space
+          if (JSON.stringify(this.wallTiles).indexOf(JSON.stringify(this.playerLocation)) !== -1 || this.playerLocation[1] == -1) {
+            this.playerLocation[1] += 1;
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
+          // If the space the player is moving into is not a wall, move player into that new space 
+          else {
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
           break;
-        case 39:
+        
         // Player pushes right arrow key
-        if(this.playerLocation[0] === (this.enemyLocation[0] - 1) && this.playerLocation[1] === this.enemyLocation[1]) {
-          console.log("enemy hit");
-        } else {
-          this.ctx.fillStyle = "black";
+        case 39:
+          // Fill in the space player is leaving with gray background color
+          this.ctx.fillStyle = "gray";
           this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-          this.playerLocation[0] = this.playerLocation[0] + 1;
-          this.ctx.fillStyle = "blue";
-          this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-        }
+          this.playerLocation[0] += 1;
+          // If the space the player is moving into is a wall, move player back to left 1 space
+          if (JSON.stringify(this.wallTiles).indexOf(JSON.stringify(this.playerLocation)) !== -1 || this.playerLocation[0] == 86) {
+            this.playerLocation[0] -= 1;
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
+          // If the space the player is moving into is not a wall, move player into that new space 
+          else {
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
           break;
-        case 40:
+        
         // Player pushes down arrow key
-        if(this.playerLocation[1] === (this.enemyLocation[1] - 1) && this.playerLocation[0] === this.enemyLocation[0]) {
-          console.log("enemy hit");
-        } else {
-          this.ctx.fillStyle = "black";
+        case 40:
+          this.ctx.fillStyle = "gray";
           this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-          this.playerLocation[1] = this.playerLocation[1] + 1;
-          this.ctx.fillStyle = "blue";
-          this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
-        }
+          this.playerLocation[1] += 1;
+          // If the space the player is moving into is a wall, move player back to left 1 space
+          if (JSON.stringify(this.wallTiles).indexOf(JSON.stringify(this.playerLocation)) !== -1 || this.playerLocation[1] == 64) {
+            this.playerLocation[1] -= 1;
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
+          // If the space the player is moving into is not a wall, move player into that new space 
+          else {
+            this.ctx.fillStyle = "green";
+            this.ctx.fillRect(this.playerLocation[0] * 10, this.playerLocation[1] * 10, 10, 10);
+          }
           break;
       }
   }
-
-
-
-colorCell(e) {
-
-    this.ctx.fillStyle = "black";
-    this.ctx.fillRect(Math.floor(e.offsetX / 10) * 10, Math.floor(e.offsetY / 10) * 10, 10, 10);
-}
 }
 
 
--- src/app/player.ts
+++ src/app/player.ts
@@ -4,4 +4,5 @@ export class Player {
   minAttack:number;
   maxAttack:number;
   weapon:string;
+  hasKey: boolean;
 }
